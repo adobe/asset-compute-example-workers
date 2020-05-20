e8d8a137080f1e0fd7ac8381bf538d7da0d3521e
dhendric <dhendric@adobe.com>
2020-05-20 15:17:22 -0600

    Added aio test dependency Changed nui cli to aio cli Added step to install asset compute AIO plugin Fixed eslint complaints

--- projects/worker-animal-pictures/worker.js
+++ projects/worker-animal-pictures/worker.js
@@ -1,4 +1,15 @@
 /*
+ * Copyright 2020 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+/*
 Copyright 2020 Adobe. All rights reserved.
 This file is licensed to you under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License. You may obtain a copy
@@ -9,7 +20,7 @@ the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTA
 OF ANY KIND, either express or implied. See the License for the specific language
 governing permissions and limitations under the License.
 */
-'use strict'
+'use strict';
 
 const { worker } = require('@adobe/asset-compute-sdk');
 const { RenditionFormatUnsupportedError } = require('@adobe/asset-compute-sdk/errors');
@@ -18,17 +29,17 @@ const urls = require('./urls');
 
 function getUrl(animal) {
     switch (animal) {
-        case 'cat':
-            return urls.cat;
-        case 'dog':
-            return urls.dog;
-        case 'elephant':
-            return urls.elephant;
-        case 'bear':
-            return urls.bear;
-        default:
-            // throw an error if animal is not valid
-            throw new RenditionFormatUnsupportedError('Invalid animal requested, must be one of: `cat`, `dog`, `elephant`, `bear`');
+    case 'cat':
+        return urls.cat;
+    case 'dog':
+        return urls.dog;
+    case 'elephant':
+        return urls.elephant;
+    case 'bear':
+        return urls.bear;
+    default:
+        // throw an error if animal is not valid
+        throw new RenditionFormatUnsupportedError('Invalid animal requested, must be one of: `cat`, `dog`, `elephant`, `bear`');
     }
 }
 
